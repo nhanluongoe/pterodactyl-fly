70a56fd6f703e1382c294004df35ace38f030c38
NhanLuong <nstball@gmail.com>
2020-06-18 11:29:01 +0700

    added clouds, trees and rockets

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ player_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("images", "
 t_rex_img_1 = pygame.transform.scale(pygame.image.load(os.path.join("images", "t-rex-1.png")), (100, 100))
 t_rex_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("images", "t-rex-2.png")), (100, 100))
 
+cloud_img_1 = pygame.transform.scale(pygame.image.load(os.path.join("images", "cloud-1.png")), (50, 50))
+cloud_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("images", "cloud-2.png")), (50, 50))
+cloud_img_3 = pygame.transform.scale(pygame.image.load(os.path.join("images", "cloud-3.png")), (50, 50))
+
+rocket_img = pygame.transform.scale(pygame.image.load(os.path.join("images", "rocket.png")), (100, 52))
+
+tree_img_1 = pygame.transform.scale(pygame.image.load(os.path.join("images", "tree-1.png")), (100, 100))
+tree_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("images", "tree-2.png")), (100, 100))
+tree_img_3 = pygame.transform.scale(pygame.image.load(os.path.join("images", "tree-3.png")), (100, 100))
+
+
+
+
 
 road_img = pygame.transform.scale(pygame.image.load(os.path.join("images", "road.png")), (2*WIDTH, int(HEIGHT/2)))
 
@@ -23,7 +36,7 @@ class Object:
         self.y = y
         self.img = []
         self.animation = True # flag for swap image to create animation effect
-        self.velocity = 0
+        self.velocity = 5
 
     def render(self, screen):
         if self.animation:
@@ -32,6 +45,9 @@ class Object:
         else:
             screen.blit(self.img[1], (self.x, self.y))
             self.animation = not self.animation
+
+    def move(self):
+        self.x -= self.velocity
 
 class Player(Object):
     def __init__(self, x, y):
@@ -48,10 +64,30 @@ class Trex(Object):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.img = [t_rex_img_1, t_rex_img_2]
-        self.velocity = 5
+        self.velocity = 7
 
-    def move(self):
-        self.x -= self.velocity
+
+class Cloud(Object):
+    def __init__(self, x, y, img):
+        super().__init__(x, y)
+        self.img = img
+
+    def render(self, screen):
+        screen.blit(self.img, (self.x, self.y))
+
+class Tree(Cloud):
+    def __init(self, x, y):
+        super().__init__(x, y)
+
+
+class Rocket(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.img = rocket_img
+        self.velocity = 10
+
+    def render(self, screen):
+        screen.blit(self.img, (self.x, self.y))
 
 class Road:
     def __init__(self, x, y):
@@ -75,7 +111,21 @@ def main():
 
     player = Player(50, 360)
     t_rexs = [Trex(WIDTH - t_rex_img_1.get_width() - 20, 340)]
+    clouds = [
+        Cloud(WIDTH - cloud_img_1.get_width() - 600, 20, cloud_img_1),
+        Cloud(WIDTH - cloud_img_1.get_width() - 20, 40, cloud_img_2),
+        Cloud(WIDTH - cloud_img_1.get_width() - 250, 80, cloud_img_3),
+        Cloud(WIDTH - cloud_img_1.get_width() - 400, 120, cloud_img_1)
+    ]
     roads = [Road(0, 240)]
+    rockets = [
+        Rocket(2*WIDTH, 40)
+    ]
+    trees = [
+        Tree(WIDTH, 330, tree_img_1),
+        Tree(WIDTH * 1.2, 330, tree_img_2),
+        Tree(WIDTH * 2.3, 330, tree_img_3)
+    ]
 
     # Initialize screen
     pygame.init()
@@ -97,18 +147,32 @@ def main():
 
         # render label
         score_label = score_font.render("Score: {}".format(math.floor(score)), 1, (0, 0, 0))
-        screen.blit(score_label, (WIDTH - score_label.get_width() - 10, 10))
+        screen.blit(score_label, (WIDTH*2, 10))
 
-        # render player
-        player.render(screen)
+        # render infinity road
+        for road in roads:
+            road.render(screen)
+        
+        # render trees
+        for tree in trees:
+            tree.render(screen)
+
+        # render clouds
+        for cloud in clouds:
+            cloud.render(screen)
 
         # render t-rex
         for t_rex in t_rexs:
             t_rex.render(screen)
 
-        # render infinity road
-        for road in roads:
-            road.render(screen)
+        # render player
+        player.render(screen)
+
+        # render rocket
+        for rocket in rockets:
+            rocket.render(screen)
+
+        
 
         pygame.display.update()
 
@@ -124,10 +188,28 @@ def main():
             if t_rex.x < -10:
                 t_rex.x = WIDTH*random.randrange(1, 3)
 
+        for cloud in clouds:
+            cloud.move()
+            if cloud.x < - 10:
+                cloud.x = WIDTH*random.randrange(1, 5)
+
+        for rocket in rockets:
+            rocket.move()
+            if rocket.x < - 10:
+                rocket.x = WIDTH*random.randrange(1, 5)
+                rocket.y = random.randrange(20, 360)
+                rocket.velocity = random.randrange(6, 15)
+
+        for tree in trees:
+            tree.move()
+            if tree.x < -10:
+                tree.x = WIDTH * random.randrange(1, 3)
+
         for road in roads:
             road.move()
             if road.x == -WIDTH:
                 roads.append(Road(WIDTH, 240))
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -138,7 +220,6 @@ def main():
             player.y += 8
         if keys[pygame.K_SPACE] and player.y > 0:
             player.jump()
-
         if (not keys[pygame.K_SPACE]) and (not keys[pygame.K_DOWN]) and player.y < 360:
             player.drown()    
 
